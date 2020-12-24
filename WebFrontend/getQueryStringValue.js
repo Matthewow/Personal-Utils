3060cd6dfa399136e41456d506972c641bfb3bdb
Matthew WANG <41960338+Matthewow@users.noreply.github.com>
2020-12-24 15:08:10 +0800

    Create getQueryStringValue.js

---
+++ WebFrontend/getQueryStringValue.js
@@ -0,0 +1,8 @@
+// using case:
+// www.abc.com/?name=Matthew&id=123
+function getQueryStringValue(name) {
+        name = name.replace(/[\[]/, "\\[").replace(/[\]]/, "\\]");
+        var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
+            results = regex.exec(location.search);
+        return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
+    }
