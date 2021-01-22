a289ae2dc71b9a103550fa6eb24d2359e6a13f25
Matthew WANG <41960338+Matthewow@users.noreply.github.com>
2021-01-22 15:17:16 +0800

    Create parser.py

---
+++ PythonTools/parser.py
@@ -0,0 +1,19 @@
+parser = argparse.ArgumentParser(description="Janus")
+    parser.add_argument("url", help="Janus root URL, e.g. http://localhost:8002")
+    parser.add_argument(
+        "--room",
+        type=int,
+        default=1234,
+        help="The video room ID to join (default: 1234).",
+    ),
+    parser.add_argument("--play-from", help="Read the media from a file and sent it."),
+    parser.add_argument("--record-to", help="Write received media to a file."),
+    parser.add_argument("--verbose", "-v", action="count")
+    parser.add_argument("--cvtype", help="CV type")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logging.basicConfig(level=logging.DEBUG)
+
+    # create signaling and peer connection
+    session = JanusSession(args.url)
